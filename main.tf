4586ee4ad6aa770ec954a4a4447d2a145371db19
Neal <bn185060@ncr.com>
2023-01-05 15:14:51 -0800

    local prov added for ssh via vscode

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "mtc_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["165.225.57.203/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -67,3 +67,39 @@ resource "aws_security_group" "mtc_sg" {
   }
 
 }
+
+resource "aws_key_pair" "mtc_auth" {
+  key_name   = "mtckey"
+  public_key = file("~/.ssh/mtckey.pub")
+}
+
+
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("windows-ssh-config.tpl", {
+      hostname     = self.public_ip,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/mtckey"
+    })
+    interpreter = ["Powershell", "-Command"]
+  }
+
+
+}
+
+
